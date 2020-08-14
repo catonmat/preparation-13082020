2c5379c6b67d1b55e4d232d3d15c4b26a1966988
richard jarram <rjarram@me.com>
2020-08-14 13:09:03 -0400

    3:4. COMPLETE. sidekiq configured.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   patch '/update_user', to: 'profiles#update', as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Sidekiq Web UI, only for admins.
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
