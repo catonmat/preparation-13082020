a92aa138109046cc112e67f2e175c9ded0af9d41
richard jarram <rjarram@me.com>
2020-08-14 17:07:21 -0400

    5:0 COMPLETE. redis-cloud setup.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,16 @@
 Redis.exists_returns_integer = false
+
+$redis = Redis.new
+
+url = ENV["REDISCLOUD_URL"]
+
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  $redis = Redis.new(:url => url)
+end
