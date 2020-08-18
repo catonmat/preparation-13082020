4509295186bedb787aad222181af838adf38e3d4
richard jarram <rjarram@me.com>
2020-08-18 15:54:17 -0400

    UPDATE. GlobalId. changed `current_user.id` to `user` in profiles_controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
     def update
         if current_user.update(user_params)
-            UpdateUserJob.perform_later(current_user.id)
+            UpdateUserJob.perform_later(current_user)
             flash[:notice] = 'Your profile has been updated'
             redirect_to root_path
         else
